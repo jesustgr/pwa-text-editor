4dd4d4d9f423c002689038586229dad2e94122df
JT Reyes <jesustgreyes@gmail.com>
2023-09-07 11:05:06 -0700

    add worokbox plugins for service worker and manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,31 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        title: 'JATE Homepage',
+        template: './index.html',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new WebpackPwaManifest({ 
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Text Editor',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        fingerprints: false,
+        icons: [
+        {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+        },
+        ],
+      }),
     ],
 
     module: {
